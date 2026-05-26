1a98d33487f36e9a56e61167931520d33267e484
Oreshnik228 <podkotilov@gmail.com>
2026-05-26 19:28:11 +0300

    innit

--- site.js
+++ site.js
@@ -1,12 +1,12 @@
-document.getElementById('nav_dog').addEventListener('click', function() {
-    var Dog = document.getElementById('Dog');
-    var Cat = document.getElementById('Cat');
-    Dog.style.display = 'block';
-    Cat.style.display = 'none';
+document.getElementById('nav_dog').addEventListener('click', () => {
+var Dog = document.getElementById('Dog');
+var Cat = document.getElementById('Cat');
+Dog.style.display = 'block';
+Cat.style.display = 'none';
 });
-document.getElementById('nav_cat').addEventListener('click', function() {
-    var Dog = document.getElementById('Dog');
-    var Cat = document.getElementById('Cat');
-    Cat.style.display = 'block';
-    Dog.style.display = 'none';
+document.getElementById('nav_cat').addEventListener('click', () => {
+var Dog = document.getElementById('Dog');
+var Cat = document.getElementById('Cat');
+Cat.style.display = 'block';
+Dog.style.display = 'none';
 });
